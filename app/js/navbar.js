57ada9f95b9ccedc85e7bfe874d0ea2a7c67e945
genecdp <102894873+genecdp@users.noreply.github.com>
2022-05-07 11:35:25 +0800

    Navbar is now generated by js.

---
+++ app/js/navbar.js
@@ -0,0 +1,13 @@
+const lang = "zh-Hant-TW";
+const navbar = document.querySelector(".navbar");
+fetch("app/js/navbarButtons.json")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(navbarButtons) {
+        let navbarButtonElements = "";
+        navbarButtons.forEach(navbarButton => {
+            navbarButtonElements += `<a href="${navbarButton["href"]}" class="nav__link"><span class="material-icons">${navbarButton["linkIcon"]}</span><span class="nav__text">${navbarButton["linkName"][lang]}</span></a>`;
+        });
+        navbar.innerHTML = navbarButtonElements;
+    });
