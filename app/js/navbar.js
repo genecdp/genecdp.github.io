0805dc405bd5f044cc9841cab0ba0a45ee511d74
genecdp <102894873+genecdp@users.noreply.github.com>
2022-05-09 17:02:49 +0800

    Migrate to Material Symbols and added some buttons

--- app/js/navbar.js
+++ app/js/navbar.js
@@ -7,7 +7,7 @@ fetch("app/js/navbarButtons.json")
     .then(function(navbarButtons) {
         let navbarButtonElements = "";
         navbarButtons.forEach(navbarButton => {
-            navbarButtonElements += `<a href="${navbarButton["href"]}" class="nav__link"><span class="material-icons">${navbarButton["linkIcon"]}</span><span class="nav__text">${navbarButton["linkName"][lang]}</span></a>`;
+            navbarButtonElements += `<a href="${navbarButton["href"]}" class="nav__link"><span class="material-symbols-rounded">${navbarButton["linkIcon"]}</span><span class="nav__text">${navbarButton["linkName"][lang]}</span></a>`;
         });
         navbar.innerHTML = navbarButtonElements;
         navbar.classList.add("display");
