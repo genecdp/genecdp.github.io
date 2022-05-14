a681dafe2263c280fa04891741f91f1630feb203
genecdp <102894873+genecdp@users.noreply.github.com>
2022-05-14 20:51:34 +0800

    Remove unnecessary js, styles and try to fix icons being indexed by search engine.

--- app/js/navbar.js
+++ app/js/navbar.js
@@ -8,7 +8,7 @@ fetch("app/js/navbarButtons.json")
     .then(function(navbarButtons) {
         let navbarButtonElements = "";
         navbarButtons.forEach(navbarButton => {
-            navbarButtonElements += `<a href="${navbarButton["href"]}" class="nav__link"><span class="material-symbols-rounded">${navbarButton["linkIcon"]}</span><span class="nav__text">${navbarButton["linkName"][lang]}</span></a>`;
+            navbarButtonElements += `<a href="${navbarButton["href"]}" class="nav__link" data-nosnippet><span class="material-symbols-rounded">${navbarButton["linkIcon"]}</span><span class="nav__text">${navbarButton["linkName"][lang]}</span></a>`;
         });
         navbar.innerHTML = navbarButtonElements;
         navbar.classList.add("display");
